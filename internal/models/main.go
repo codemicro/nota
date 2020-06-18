121d4427bd4e1a8cba003fbd203e4fcf0da98109
Tom <tom@tdpain.net>
2020-06-18 14:59:14 +0100

    Update models

--- internal/models/main.go
+++ internal/models/main.go
@@ -1,7 +1,9 @@
 package models
 
+import "github.com/jinzhu/gorm"
 
 type Session struct {
+	gorm.Model
 	Time int32 "json:`time`"
 	Subject string "json:`subject`"
 	Files []File "json:`files`"
@@ -10,6 +12,13 @@ type Session struct {
 
 
 type File struct {
+	gorm.Model
 	Name string "json:`name`"
 	Path string "json:`path`"
+}
+
+
+type GenericResponse struct {
+	Status string "json:`status`"
+	Message string "json:`message`"
 }

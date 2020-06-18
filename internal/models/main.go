a3098e439f258663331e4f27444a0ac680d52561
Tom <tom@tdpain.net>
2020-06-18 20:13:03 +0100

    Tweak models
    Also run go fmt

--- internal/models/main.go
+++ internal/models/main.go
@@ -2,23 +2,28 @@ package models
 
 import "github.com/jinzhu/gorm"
 
+// Database models
 type Session struct {
 	gorm.Model
-	Time int32 "json:`time`"
-	Subject string "json:`subject`"
-	Files []File "json:`files`"
-	Title string "json:`title`"
+	Time    int32  `json:"time"`
+	Subject string `json:"subject"`
+	Title   string `json:"title"`
 }
-
 
 type File struct {
 	gorm.Model
-	Name string "json:`name`"
-	Path string "json:`path`"
+	Name          string `json:"name" gorm:"unique;"`
+	Path          string `json:"path"`
+	ParentSession int64  `json:"parentSession"`
 }
 
-
+// Response models
 type GenericResponse struct {
-	Status string "json:`status`"
-	Message string "json:`message`"
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+type SessionWithFiles struct {
+	Session
+	Files []File `json:"files"`
 }

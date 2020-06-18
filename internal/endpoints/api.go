611a5c250928b1be99ad64fa2492959ca0bc072e
Tom <tom@tdpain.net>
2020-06-18 21:45:08 +0100

    Update endpoints and helpers

--- internal/endpoints/api.go
+++ internal/endpoints/api.go
@@ -2,26 +2,27 @@ package endpoints
 
 import (
 	"github.com/codemicro/nota/internal/database"
+	"github.com/codemicro/nota/internal/helpers"
 	"github.com/codemicro/nota/internal/models"
 	"github.com/gofiber/fiber"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"strconv"
 )
 
+var (
+	allowableMimeTypes = []string{"image/jpeg", "image/png"}
+)
 
-func apiCreateSession(c *fiber.Ctx) {
-	// TODO: Parse values
+// Session functions
+func apiGetAllSessions(c *fiber.Ctx) {
 	conn := database.Conn
-	session := models.Session{
-		Time:    1592491944,
-		Subject: "Maths",
-		Title:   "Quadratic equations",
-	}
+	var sessions []models.Session
+	conn.Find(&sessions)
 
-	conn.Create(&session)
-
-	c.JSON(session)
+	c.JSON(sessions)
 }
-
 
 func apiGetSession(c *fiber.Ctx) {
 	conn := database.Conn
@@ -54,6 +55,47 @@ func apiGetSession(c *fiber.Ctx) {
 	})
 }
 
+func apiCreateSession(c *fiber.Ctx) {
+	// TODO: Parse values
+	conn := database.Conn
+	session := models.Session{
+		Time:    1592491944,
+		Subject: "Maths",
+		Title:   "Quadratic equations",
+	}
+
+	conn.Create(&session)
+
+	c.JSON(session)
+}
+
+// File functions
+func apiGetAllFiles(c *fiber.Ctx) {
+
+	id, err := strconv.ParseInt(c.Params("id"), 10, 64)
+	if err != nil {
+		c.Status(400).JSON(models.GenericResponse{
+			Status:  "error",
+			Message: "ID is not a valid number",
+		})
+		return
+	}
+
+	conn := database.Conn
+
+	if conn.Find(&models.Session{}, id).RecordNotFound() {
+		c.Status(404).JSON(models.GenericResponse{
+			Status:  "error",
+			Message: "Session ID not found",
+		})
+		return
+	}
+
+	var files []models.File
+	conn.Where(&models.File{ParentSession: id}).Find(&files)
+
+	c.JSON(files)
+}
 
 func apiAddFile(c *fiber.Ctx) {
 	// TODO: Support file upload
@@ -83,10 +125,8 @@ func apiAddFile(c *fiber.Ctx) {
 	conn := database.Conn
 
 	// check there's a session with the specified ID
-
-	var sessions []models.Session
 	var count int
-	conn.Model(&sessions).Where("id = ?", int(id)).Count(&count)
+	conn.Model(&models.Session{}).Where("id = ?", int(id)).Count(&count)
 
 	if count == 0 {
 		c.Status(404).JSON(models.GenericResponse{
@@ -96,23 +136,70 @@ func apiAddFile(c *fiber.Ctx) {
 		return
 	}
 
-	file := models.File{
+	// Get multipart image
+	imageFile, err := c.FormFile("image")
+	if err != nil {
+		c.Status(400).JSON(models.GenericResponse{
+			Status:  "error",
+			Message: "No 'image' included",
+		})
+		return
+	}
+
+	// Check file is an image
+	fileHandler, _ := imageFile.Open()
+	fileCont, err := ioutil.ReadAll(fileHandler)
+
+	mimeType := http.DetectContentType(fileCont)
+
+	if !helpers.IsStringInSlice(mimeType, allowableMimeTypes) {
+		c.Status(400).JSON(models.GenericResponse{
+			Status:  "error",
+			Message: "Bad image format",
+		})
+		return
+	}
+
+	// Determine image path
+	var filePath string
+	for {
+		var fileExt string
+		fileExt, err = helpers.GetFileExtension(filename)
+		if err != nil {
+			fileExt = "jpg"
+		}
+		randomFilename, _ := helpers.RandomHex(20)
+		filePath = randomFilename + "." + fileExt
+
+		var num int
+		conn.Model(&models.File{}).Where(&models.File{Path: filePath}).Count(&num)
+
+		if num == 0 {
+			break
+		}
+	}
+
+	filePath = "img" + string(os.PathSeparator) + filePath
+
+	err = helpers.SaveBytesToDisk(filePath, fileCont)
+
+	if err != nil {
+		c.Status(500).JSON(models.GenericResponse{
+			Status:  "error",
+			Message: "Internal server error - check back later",
+		})
+		return
+	}
+
+	responseObject := models.File{
 		Name:  filename,
-		Path:  filename,
+		Path:  filePath,
 		ParentSession: id,
 	}
 
 	// Insert
 
-	conn.Create(&file)
+	conn.Create(&responseObject)
 
-	c.JSON(file)
-}
-
-func apiGetAllSessions(c *fiber.Ctx) {
-	conn := database.Conn
-	var sessions []models.Session
-	conn.Find(&sessions)
-
-	c.JSON(sessions)
+	c.JSON(responseObject)
 }
--- internal/helpers/main.go
+++ internal/helpers/main.go
@@ -3,12 +3,49 @@ package helpers
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"errors"
+	"os"
+	"strings"
 )
 
-func randomHex(n int) (string, error) {
+func RandomHex(n int) (string, error) {
 	bytes := make([]byte, n)
 	if _, err := rand.Read(bytes); err != nil {
 		return "", err
 	}
 	return hex.EncodeToString(bytes), nil
+}
+
+
+func GetFileExtension(s string) (string, error) {
+	if !strings.ContainsAny(s, ".") {
+		return "", errors.New("no points found in input (impossible to parse)")
+	}
+
+	splitString := strings.Split(s, ".")
+	return splitString[len(splitString)- 1], nil
+
+}
+
+func IsStringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func SaveBytesToDisk(path string, content []byte) error {
+	f, err := os.Create(path)
+	defer f.Close()
+	if err != nil{
+		return err
+	}
+	_, err = f.Write(content)
+	if err != nil{
+		return err
+	}
+
+	return nil
 }

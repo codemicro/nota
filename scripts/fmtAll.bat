ac85c16d61aa035b3e632278f99d214a8357c53d
Tom <tom@tdpain.net>
2020-06-19 21:49:04 +0100

    Actually `go fmt github.com/codemicro/nota/...` does this for me

--- scripts/fmtAll.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-go fmt github.com/codemicro/nota/cmd/nota
-go fmt github.com/codemicro/nota/internal/endpoints
-go fmt github.com/codemicro/nota/internal/models
-go fmt github.com/codemicro/nota/internal/helpers
-go fmt github.com/codemicro/nota/internal/database

a180b986f72b14510e0640458d54bc5ab7696bc1
Tom <tom@tdpain.net>
2020-06-19 21:47:46 +0100

    Fancy script to format everything

---
+++ scripts/fmtAll.bat
@@ -0,0 +1,6 @@
+@echo off
+go fmt github.com/codemicro/nota/cmd/nota
+go fmt github.com/codemicro/nota/internal/endpoints
+go fmt github.com/codemicro/nota/internal/models
+go fmt github.com/codemicro/nota/internal/helpers
+go fmt github.com/codemicro/nota/internal/database

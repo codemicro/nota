17f4336e5eabe3dbd7a2a5e626d7b66347011c71
Tom <tom@tdpain.net>
2020-06-21 14:56:18 +0100

    Add error and 404 handling

--- cmd/nota/main.go
+++ cmd/nota/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/codemicro/nota/internal/database"
+	"github.com/codemicro/nota/internal/models"
 	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/middleware"
 	"log"
 	"os"
 
@@ -17,10 +19,33 @@ var (
 func main() {
 	app := fiber.New()
 
+	app.Use(middleware.Recover()) // Recover from any panic within the rest of the app
+
 	app.Use(func(c *fiber.Ctx) {
 		c.Append("X-Powered-By", "Nota v"+version)
 		c.Next()
 	})
+
+	app.Use(func(c *fiber.Ctx) {
+		c.Status(fiber.StatusNotFound).JSON(models.GenericResponse{
+			Status:  "error",
+			Message: "404 - resource not found",
+		})
+	})
+
+	app.Settings.ErrorHandler = func(ctx *fiber.Ctx, err error) {
+		code := fiber.StatusInternalServerError
+
+		if e, ok := err.(*fiber.Error); ok {
+			code = e.Code
+		}
+
+		// Return HTTP response
+		ctx.Status(code).JSON(models.GenericResponse{
+			Status:  "error",
+			Message: err.Error(),
+		})
+	}
 
 	database.InitDatabase()
 	endpoints.InitEndpoints(app)

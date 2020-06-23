d8ebafa51b7b092180f5fd7341b4863b22c5419c
Tom <tom@tdpain.net>
2020-06-23 17:37:09 +0100

    Tweak key generation script

--- scripts/generateKeyPair.sh
+++ scripts/generateKeyPair.sh
@@ -3,3 +3,4 @@ mkdir -p ../run
 echo "Ensure you set NO PASSPHRASE"
 ssh-keygen -t rsa -b 4096 -m PEM -f ../run/private.key
 openssl rsa -in ../run/private.key -pubout -outform PEM -out ../run/public.key
+rm ../run/private.key.pub

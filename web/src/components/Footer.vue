da141460e4f45f1ddd727f30e12d093c1414f822
Tom <tom@tdpain.net>
2020-06-24 14:18:08 +0100

    I don't know what I've done but in all honesty this is looking promising

---
+++ web/src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="footer">
+        <br><hr>
+        <p>Powered by <a href="https://www.github.com/codemicro/nota" target="_blank" rel="noopener">Nota</a> - MPL-2.0
+            <br>
+            Nota icon made by Freepik from flaticon.com
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+    .footer {
+        color: gray;
+        font-size: 12px;
+    }
+
+    hr {
+        display: block;
+        height: 1px;
+        border: 0;
+        border-top: 1px solid #ccc;
+        margin: 1em 0;
+        padding: 0;
+    }
+</style>
---
+++ web/src/pages/Home/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app" class="container">
+
+        <div class="spacer" />
+
+        <img alt="Nota logo" src="@/assets/logo.svg" width="20%">
+
+        <h1>Welcome to Nota</h1>
+
+        <nav>
+            <ul>
+                <li>Go to</li>
+                <li>
+                    <a href="about.html">About Us</a>
+                </li>
+            </ul>
+        </nav>
+
+        <Footer />
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        components: {
+            Footer: () => import("@/components/Footer.vue")
+        }
+    }
+</script>
+
+<style scoped>
+    @import "../../assets/common.css";
+</style>
